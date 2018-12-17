5d2e79c60b0388fd27f1fa213b9f487b82d68f6f
Nikita <pishkin1112@gmail.com>
2018-12-17 23:13:09 +0300

    Третий коммит. Заголовки, и App

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import './App.css';
 //Добавляем эл-ы из BS для  react из документации ES6
 //Добавляем контейнер, ряд, и колонки
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 //импортируем именно таким способом для усорения скорости загрузки и 
 //экономии память, тк таким способом импортируется только те элементы
 //и ничего лишнего
 //другой способ import { Grid } from 'react-bootstrap'; - более обширный
-
-
 import styled from 'styled-components'
+
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
 
 
 class App extends Component {
   render() {
     return (
+      <div className="App">
+        <Header />
+        <Menu />
+        <Main />
+      </div>
       
 
     );
