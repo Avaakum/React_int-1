bc576fa93b523b37197edae4e69a3fc70006fdca
Nikita <pishkin1112@gmail.com>
2018-12-17 21:28:10 +0300

    Первый коммит

--- src/App.js
+++ src/App.js
@@ -2,27 +2,61 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Добавляем эл-ы из BS для  react из документации ES6
+//Добавляем контейнер, ряд, и колонки
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+//импортируем именно таким способом для усорения скорости загрузки и 
+//экономии память, тк таким способом импортируется только те элементы
+//и ничего лишнего
+//другой способ import { Grid } from 'react-bootstrap'; - более обширный
+
+
+import styled from 'styled-components'
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      //ВНимание класс пишется в формате camelCase
+      <div className="wrapper">
+        <SayFullName name="Nik" surname="Chops" link="ya.ru" />
+        <SayFullName name="Pop" surname="Tuk" link="vk.su" />
+        <SayFullName name="Ted" surname="Rol" link="sp.du" />
       </div>
     );
   }
 }
+
+
+
+
+function SayFullName (props) {
+  return (
+      <div>
+      <h1> Мое имя {props.name}, фамилия - {props.surname} </h1>
+      <a href={props.link}> Ссылка на мой профиль </a>
+      </div>
+    )
+}
+
+
+
+//Создаем ф-юю для использования одного и того же компонента с разными атрибутами
+// function MeetAll() {
+//   return (
+      
+//     )
+// }
+//Переместили всё из этой функции внутрь App
+
+
+
+//Переместили функцию в константу, что-бы сократить длину функции рендеринга и
+//лишний раз разбить на части
+// const element = <SayFullName name="Nik" surname="Chops" link="ya.ru" />
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+function Hello() {
+  let phrase = "World"
+  return (
+    <h1> Hello {phrase+5}!</h1>
+    //Да-да в фигурные скобки можно писать что угодно и он поймет
+    )
+}
+
+
+//Обычно структура именно такая  и в этом файле ничего кроме рендера нетуу
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
